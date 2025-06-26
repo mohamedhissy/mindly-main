bb597d2b6f05e9a7d42791043197f6285c2d8410
mohamedhissy <mhmdalhsy49@gmail.com>
2025-06-26 16:09:03 +0300

    Automatic update: Changed container color to pink

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class HomeView extends StatelessWidget {
         children: [
           SafeArea(
             child: Container(
-              color: Colors.orange,
+              color: Colors.pink,
               child: Center(child: Text('data')),
           // إضافة Container جديد هنا
           Container(
