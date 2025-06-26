bc2e9cda3d0ea83b3deb8ab87ea5958a53a405de
mohamedhissy <mhmdalhsy49@gmail.com>
2025-06-26 16:13:44 +0300

    Automatic update: Changed container color to cyan

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class HomeView extends StatelessWidget {
         children: [
           SafeArea(
             child: Container(
-              color: Colors.purple,
+              color: Colors.cyan,
               child: Center(child: Text('data')),
           // إضافة Container جديد هنا
           Container(
