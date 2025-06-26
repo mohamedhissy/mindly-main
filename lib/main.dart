6cc7aa1896149113c763256dd07387880c68bec6
mohamedhissy <mhmdalhsy49@gmail.com>
2025-06-26 16:20:33 +0300

    Automatic update: Changed container color to blue

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class HomeView extends StatelessWidget {
         children: [
           SafeArea(
             child: Container(
-              color: Colors.cyan,
+              color: Colors.blue,
               child: Center(child: Text('data')),
           // إضافة Container جديد هنا
           Container(
