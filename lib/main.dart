286bb858116d282e301a078dcde23e83d69293ef
mohamedhissy <mhmdalhsy49@gmail.com>
2025-06-26 15:46:32 +0300

    Automatic update: Added new container

--- lib/main.dart
+++ lib/main.dart
@@ -97,3 +97,10 @@ class HomeView extends StatelessWidget {
             color: Colors.blue,
             child: Center(child: Text('New Container')),
           ),
+          // إضافة Container جديد هنا
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.blue,
+            child: Center(child: Text('New Container')),
+          ),
